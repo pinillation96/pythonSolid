669c42c35e44ff1a7a05a29f03bc53dc30874e11
pinillation96 <44420238+pinillation96@users.noreply.github.com>
2023-04-20 13:09:25 -0600

    update

--- README.md
+++ README.md
@@ -35,3 +35,38 @@ This will create a docker image using the `Dockerfile` with the image name `YOUR
 Run container
 
     >docker run YOUR_NAME
+
+
+## readmeSOLID
+    # IMDB Top 250 Movies Scraper
+
+Este script en Python se encarga de raspar la información de las 250 películas mejor calificadas en IMDb utilizando `BeautifulSoup` y guarda los resultados en un archivo CSV. El script sigue los principios SOLID para garantizar un código limpio y fácil de mantener.
+
+## Principios SOLID aplicados
+
+### S - Principio de responsabilidad única
+
+La clase `IMDBScraper` encapsula toda la funcionalidad relacionada con el raspado y procesamiento de datos de las películas. Se crean métodos separados para cada tarea específica (`fetch_data`, `parse_data`, `save_to_csv`), garantizando que cada método tenga una única responsabilidad.
+
+### O - Principio abierto/cerrado
+
+El método `scrape` en la clase `IMDBScraper` utiliza los métodos `fetch_data` y `parse_data` para realizar la funcionalidad de raspado sin modificar su implementación. Si es necesario extender la funcionalidad de raspado, se pueden modificar o agregar nuevos métodos a la clase `IMDBScraper` sin afectar su implementación actual.
+
+### D - Principio de inversión de dependencia
+
+La clase `IMDBScraper` utiliza inyección de dependencia en su constructor para recibir la URL como argumento. Esto permite a la clase ser independiente de detalles concretos y hace que sea más fácil de modificar o reutilizar en diferentes contextos.
+
+### L - Principio de sustitución de Liskov
+
+No hay un caso claro en el script donde se aplique el principio de sustitución de Liskov, ya que no se utilizan clases heredadas ni polimorfismo.
+
+### I - Principio de segregación de interfaces
+
+Tampoco hay un caso claro en el script donde se aplique el principio de segregación de interfaces, ya que no se utilizan interfaces ni clases abstractas.
+
+## Uso
+
+Para ejecutar el script, simplemente ejecuta el siguiente comando en tu terminal:
+
+```bash
+python imdb_scraper.py
--- src/movie_fetcher.py
+++ src/movie_fetcher.py
@@ -1,52 +1,113 @@
+# import requests
+# import re
+# import csv
+# from bs4 import BeautifulSoup
+
+
+# def main():
+#     Downloading imdb top 250 movie's data
+#     url = 'http://www.imdb.com/chart/top'
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, 'lxml')
+
+#     movies = soup.select('td.titleColumn')
+#     links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
+#     crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
+#     ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
+#     votes = [b.attrs.get('data-value') for b in soup.select('td.ratingColumn strong')]
+
+#     create a empty list for storing
+#     movie information
+#     list = []
+
+#     Iterating over movies to extract
+#     each movie's details
+#     for index in range(0, len(movies)):
+#         Separating movie into: 'place',
+#         'title', 'year'
+#         movie_string = movies[index].get_text()
+#         movie = (' '.join(movie_string.split()).replace('.', ''))
+#         movie_title = movie[len(str(index)) + 1:-7]
+#         year = re.search('\((.*?)\)', movie_string).group(1)
+#         place = movie[:len(str(index)) - (len(movie))]
+
+#         data = {"movie_title": movie_title,
+#                 "year": year,
+#                 "place": place,
+#                 "star_cast": crew[index],
+#                 "rating": ratings[index],
+#                 "vote": votes[index],
+#                 "link": links[index],
+#                 "preference_key": index % 4 + 1}
+#         list.append(data)
+
+#     fields = ["preference_key", "movie_title", "star_cast", "rating", "year", "place", "vote", "link"]
+#     with open("movie_results.csv", "w", newline="") as file:
+#         writer = csv.DictWriter(file, fieldnames=fields)
+#         writer.writeheader()
+#         for movie in list:
+#             writer.writerow({**movie})
+
+
+# if __name__ == '__main__':
+#     main()
 import requests
 import re
 import csv
 from bs4 import BeautifulSoup
 
 
+class IMDBScraper:
+    def __init__(self, url):
+        self.url = url
+        self.movies = []
+
+    def fetch_data(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        return soup
+
+    def parse_data(self, soup):
+        movies = soup.select('td.titleColumn')
+        links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
+        crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
+        ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
+        votes = [b.attrs.get('data-value') for b in soup.select('td.ratingColumn strong')]
+
+        for index in range(len(movies)):
+            movie_string = movies[index].get_text()
+            movie = (' '.join(movie_string.split()).replace('.', ''))
+            movie_title = movie[len(str(index)) + 1:-7]
+            year = re.search('\((.*?)\)', movie_string).group(1)
+            place = movie[:len(str(index)) - (len(movie))]
+
+            data = {"movie_title": movie_title,
+                    "year": year,
+                    "place": place,
+                    "star_cast": crew[index],
+                    "rating": ratings[index],
+                    "vote": votes[index],
+                    "link": links[index],
+                    "preference_key": index % 4 + 1}
+            self.movies.append(data)
+
+    def save_to_csv(self, filename):
+        fields = ["preference_key", "movie_title", "star_cast", "rating", "year", "place", "vote", "link"]
+        with open(filename, "w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=fields)
+            writer.writeheader()
+            for movie in self.movies:
+                writer.writerow(movie)
+
+    def scrape(self):
+        soup = self.fetch_data()
+        self.parse_data(soup)
+
+
 def main():
-    # Downloading imdb top 250 movie's data
-    url = 'http://www.imdb.com/chart/top'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    movies = soup.select('td.titleColumn')
-    links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
-    crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
-    ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
-    votes = [b.attrs.get('data-value') for b in soup.select('td.ratingColumn strong')]
-
-    # create a empty list for storing
-    # movie information
-    list = []
-
-    # Iterating over movies to extract
-    # each movie's details
-    for index in range(0, len(movies)):
-        # Separating movie into: 'place',
-        # 'title', 'year'
-        movie_string = movies[index].get_text()
-        movie = (' '.join(movie_string.split()).replace('.', ''))
-        movie_title = movie[len(str(index)) + 1:-7]
-        year = re.search('\((.*?)\)', movie_string).group(1)
-        place = movie[:len(str(index)) - (len(movie))]
-
-        data = {"movie_title": movie_title,
-                "year": year,
-                "place": place,
-                "star_cast": crew[index],
-                "rating": ratings[index],
-                "vote": votes[index],
-                "link": links[index],
-                "preference_key": index % 4 + 1}
-        list.append(data)
-
-    fields = ["preference_key", "movie_title", "star_cast", "rating", "year", "place", "vote", "link"]
-    with open("movie_results.csv", "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=fields)
-        writer.writeheader()
-        for movie in list:
-            writer.writerow({**movie})
+    imdb_scraper = IMDBScraper('http://www.imdb.com/chart/top')
+    imdb_scraper.scrape()
+    imdb_scraper.save_to_csv("movie_results.csv")
 
 
 if __name__ == '__main__':
